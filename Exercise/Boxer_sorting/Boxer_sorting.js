2396c42373747f8d6c147abc7d5193be61907cb7
iyu88 <31645195+iyu88@users.noreply.github.com>
2021-09-10 11:39:58 +0900

    Update Boxer_sorting.js

--- Exercise/Boxer_sorting/Boxer_sorting.js
+++ Exercise/Boxer_sorting/Boxer_sorting.js
@@ -7,17 +7,18 @@ function solution(w, h2h) {
         return [!(win / (m.length - n)) ? 0 : win / (m.length - n), u, i];
     }).sort((a, b) => {
         if (a[0] !== b[0]) {
-            return b[0] - a[0] 
+            return b[0] - a[0] // this line
         } else if (a[0] === b[0]) {
             if ( a[1] !== b[1] ) {
-                return b[1] - a[1]
+                return b[1] - a[1] // this line
             } else if ( a[1] === b[1] ) {
                 if ( w[a[2]] !== w[b[2]]) {
-                    return w[b[2]] - w[a[2]]
+                    return w[b[2]] - w[a[2]] // this line
                 } else {
-                    return a[2] - b[2]
+                    return a[2] - b[2] // this line
                 }
             }
         }
+    // .sort((a, b) =>  b[0] - a[0] || b[1] - a[1] || w[b[2]] - w[a[2]] || a[2] - b[2])
     }).map((answers) => answers[2] + 1);
 }
