bf0071880d76f7a0959a4034a2e52ee71a3f4566
iyu88 <iyu88@naver.com>
2022-10-13 23:55:50 +0900

    1013: String

---
+++ BaekJoon/String/YourHandle/README.md
@@ -0,0 +1,5 @@
+## 백준
+
+### > 15819 : String
+
+[너의 핸들은](https://www.acmicpc.net/problem/15819)
---
+++ BaekJoon/String/YourHandle/YourHandle.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+let [nums, ...arr] = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+let [N, M] = nums.split(" ").map(Number);
+console.log(arr.sort()[M - 1]);
