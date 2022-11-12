8112b7b43c1e132e2156614a99efd71901896483
iyu88 <iyu88@naver.com>
2022-11-12 23:56:52 +0900

    1112: String

---
+++ BaekJoon/String/Ah/Ah.js
@@ -0,0 +1,4 @@
+const fs = require("fs");
+let [a, b] = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+console.log(a.length >= b.length ? "go" : "no");
---
+++ BaekJoon/String/Ah/README.md
@@ -0,0 +1,5 @@
+## 백준
+
+### > 4999 : String
+
+[아!](https://www.acmicpc.net/problem/4999)
