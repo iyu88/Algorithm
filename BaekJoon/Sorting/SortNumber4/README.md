3ddc71f36c02be9ee49e2e6dd452e26a0ffa23b3
iyu88 <iyu88@naver.com>
2022-12-28 15:46:31 +0900

    1228: Sorting

---
+++ BaekJoon/Sorting/SortNumber4/README.md
@@ -0,0 +1,3 @@
+## 백준
+### > 11931 : Sorting
+[수 정렬하기 4](https://www.acmicpc.net/problem/11931)
---
+++ BaekJoon/Sorting/SortNumber4/SortNumber4.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const [N, ...arr] = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(Number);
+
+const divide = (arr, low, high) => {
+    const pivot = arr[Math.floor((low + high) / 2)];
+    
+    while (low <= high) {
+        while (arr[low] > pivot) low++;
+        while (arr[high] < pivot) high--;
+        if (low <= high) {
+            [arr[low], arr[high]] = [arr[high], arr[low]];
+            low++;
+            high--;
+        }
+    }
+    
+    return low;
+}
+
+const quick_sort = (arr, low = 0, high = arr.length - 1) => {
+    if (low >= high) return arr;
+    if (arr.length < 2) return arr;
+    
+    const divided = divide(arr, low, high);
+    
+    quick_sort(arr, low, divided - 1);
+    quick_sort(arr, divided, high);
+    
+    return arr;
+}
+
+console.log(quick_sort(arr).join('\n'));
