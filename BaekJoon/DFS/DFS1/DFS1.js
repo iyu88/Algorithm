ebefe3bfb66410a515f09a407e1b9216ef87bc8a
iyu88 <iyu88@naver.com>
2023-01-05 23:02:03 +0900

    0105: DFS/DFS

---
+++ BaekJoon/DFS/DFS1/DFS1.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const [nums, ...arr] = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(el => el.split(' ').map(Number));
+
+const [N, M, R] = nums;
+let count = 0;
+const visited = Array(N+1).fill(0);
+const points = Array(N+1).fill(null).map(_ => []);
+arr.forEach(el => {
+    const [from, to] = el;
+    points[from].push(to);
+    points[to].push(from);
+});
+
+const sortedPoints = points.map(el => el.sort((a, b) => a - b));
+
+const dfs = (x) => {
+    if (!visited[x]) {
+        visited[x] = ++count;
+        for (const next of points[x]) {
+            dfs(next);
+        }
+    }
+}
+
+dfs(R)
+
+console.log(visited.slice(1).join('\n'));
---
+++ BaekJoon/DFS/DFS1/README.md
@@ -0,0 +1,3 @@
+## 백준
+### > 24479 : DFS
+[알고리즘 수업 - 깊이 우선 탐색 1](https://www.acmicpc.net/problem/24479)
