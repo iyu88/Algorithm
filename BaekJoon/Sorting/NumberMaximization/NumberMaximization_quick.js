5f3443569d9750990b2682014c7f044c2041f495
iyu88 <iyu88@naver.com>
2023-05-20 22:55:02 +0900

    0520: Sorting

---
+++ BaekJoon/Sorting/NumberMaximization/NumberMaximization_quick.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const num = fs.readFileSync('/dev/stdin').toString().trim().split('').map(Number);
+
+const divide = (arr, left, right) => {
+    const pivot = arr[Math.floor((left + right) / 2)];
+    
+    while (left <= right) {
+        while (arr[left] > pivot) left++;
+        while (arr[right] < pivot) right--;
+        if (left <= right) {
+            [arr[left], arr[right]] = [arr[right], arr[left]];
+            left++;
+            right--;
+        }
+    }
+    
+    return left;
+}
+
+const quickSort = (arr, left = 0, right = arr.length - 1) => {
+    if (left >= right) return arr;
+    if (arr.length < 2) return arr;
+    
+    const divided = divide(arr, left, right);
+    
+    quickSort(arr, left, divided-1);
+    quickSort(arr, divided, right);
+    
+    return arr;
+}
+
+console.log(quickSort(num).join(''));
---
+++ BaekJoon/Sorting/NumberMaximization/README.md
@@ -0,0 +1,3 @@
+## 백준
+### > 25773 : Sorting
+[Number Maximization](https://www.acmicpc.net/problem/25773)
